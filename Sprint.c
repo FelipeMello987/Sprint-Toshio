8cf449f28b6b0cc69a6405d38842087826de23b5
felipemello987 <felipemello200518@gmail.com>
2026-04-25 01:17:45 -0300

    adição de while para validaçao de resposta // limpeza do codigo para melhor compreensão

--- Sprint.c
+++ Sprint.c
@@ -2,16 +2,24 @@
 #include <string.h>
 
 int main() {
-    int energia, hora, minuto, recarregada;
+    int energia, hora, minuto, recarregada, valido;
     float custo;
     char vip[10];
 
-    printf("========== SIMULADOR DE RECARGA ==========\n");
-    printf("Preco da recarga e R$0,40 para cada 1%% de recarga\n");
-    printf("Voce e cliente premium para ganhar 10%% de desconto?[S/N] ");
-    scanf("%s", vip);
-    printf("Qual a carga atual em porcentagem: ");
-    scanf("%d", &energia);
+    valido = 1;
+    while (valido) {
+        printf("========== SIMULADOR DE RECARGA ==========\n");
+        printf("Preco da recarga e R$0,40 para cada 1%% de bateria faltante\n");
+        printf("Qual a carga atual em porcentagem: ");
+        scanf("%d", &energia);
+        printf("Voce e cliente premium para ganhar 10%% de desconto?[S/N] ");
+        scanf("%s", vip);
+        if (strcmp(vip, "s") != 0 && strcmp(vip, "n") != 0) {
+            printf("erro: resposta invalida\n");
+        } else {
+            valido = 0;
+        }
+    }
 
     hora = 0;
     minuto = -1;
@@ -20,8 +28,7 @@ int main() {
 
     if (energia >= 100){
         energia = 99;
-    }
-    if (energia < 0){
+    } else if (energia < 0){
         energia = 0;
     }
 
@@ -42,8 +49,7 @@ int main() {
     }   
 
     printf("========== RECARGA COMPLETA ==========\n");
-    printf("Tempo gasto: %d hora(s)", hora);
-    printf(" e %d minuto(s)\n",minuto);
+    printf("Tempo gasto: %d hora(s) e %d minuto(s)\n", hora, minuto);
     printf("Energia recarregada: %d%%\n", recarregada);
     printf("Valor a se pagar: R$%.2f", custo);
     return 0;
